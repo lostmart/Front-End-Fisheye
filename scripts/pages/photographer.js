9c28cb31dc883144afdd9aee1b2a56b650267a38
lostmart <lostmart@hotmail.com>
2022-12-11 11:41:41 +0100

    responsiveness

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,34 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+// DOM slements
+const toggleBtn = document.querySelectorAll('li')[0]
+const listUl = document.querySelectorAll('ul')[0]
+const bodyClick = document.querySelector('body')
+
+let openList = false
+toggleBtn.addEventListener('click', (e) => {
+	if (!openList) {
+		e.stopPropagation()
+		listUl.classList.add('open-list')
+		toggleBtn.childNodes[1].classList.add('open-item')
+		toggleBtn.style.borderBottomColor = 'white'
+		listUl.childNodes[3].style.borderBottomColor = 'white'
+		openList = true
+	} else {
+		closeList()
+	}
+})
+
+bodyClick.addEventListener('click', () => {
+	if (openList === true) {
+		closeList()
+	}
+})
+
+function closeList() {
+	listUl.classList.remove('open-list')
+	toggleBtn.childNodes[1].classList.remove('open-item')
+	toggleBtn.style.borderBottomColor = 'transparent'
+	listUl.childNodes[3].style.borderBottomColor = 'transparent'
+	openList = false
+}
