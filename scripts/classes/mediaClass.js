978c9ffa0f20d529f5acfde96ea8eccb0bbc7cc7
lostmart <lostmart@hotmail.com>
2022-12-17 13:04:37 +0100

    new

--- scripts/classes/mediaClass.js
+++ scripts/classes/mediaClass.js
@@ -1,4 +1,4 @@
-export default class Photographer {
+export default class Photo {
 	constructor({ id, photographerId, title, image, likes, date, price }) {
 		this.id = id
 		this.photographerId = photographerId
@@ -10,5 +10,33 @@ export default class Photographer {
 	}
 	displayInfo() {
 		console.log(`the title is: ${this.title}`)
+	}
+	folderName() {
+		console.log(this.photographerId)
+		let folder = ''
+		switch (this.photographerId) {
+			case 930:
+				folder = 'Ellie-Rose'
+				break
+			case 243:
+				folder = 'Mimi'
+				break
+			case 82:
+				folder = 'Tracy'
+				break
+			case 527:
+				folder = 'Nabeel'
+				break
+			case 925:
+				folder = 'Rhode'
+				break
+			case 195:
+				folder = 'Marcel'
+				break
+			default:
+				folder = null
+			// code block
+		}
+		return `images/${folder}/`
 	}
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,10 +7,10 @@ import {
 	textBlock,
 	getContElemCont,
 } from '../factories/createDomElems.js'
-/* create photographer based in class  */
+/* create photographer based in photographer class  */
 import Photographer from '../classes/photographerClass.js'
-/* create photographer based in class  */
-import MediaData from '../classes/mediaClass.js'
+/* create media array of objects based in media class  */
+import Photo from '../classes/mediaClass.js'
 
 const url =
 	'https://lostmart.github.io/Front-End-Fisheye/data/photographers.json'
@@ -66,16 +66,19 @@ async function init() {
 	const newData = new getData(url)
 	const { media, photographers } = await newData.brigMeData()
 
-	// rederDataDom(selectedPhotographer(photographers))
-	// new oobject from photographer class
-
+	/*  PHOTOGRAPHER CONSTRUCT */
 	const photographerModel = photographerFactory(
 		new Photographer(selectedPhotographer(photographers))
 	)
 	photographerModel.singlePageHeader(photographerModel, mainCont)
 
-	// const photographer = new mediaFactory(photographers[0])
-	// photographer.photographHeader()
+	/*  PHOTO CONSTRUCT    */
+	const newPhotoModel = new Photo(media[21])
+	console.log(newPhotoModel)
+	console.log(newPhotoModel.folderName())
+
+	//const photographer = new mediaFactory(photographers[0])
+	//photographer.photographHeader()
 
 	const filteredPhotos = arrangeData(media)
 	console.log(filteredPhotos)
@@ -84,68 +87,6 @@ async function init() {
 	//console.log(newdata)
 }
 
-/*
-function rederDataDom(photographer) {
-	// console.log(photographer)
-	// creation
-	const photographerModel = photographerFactory(new Photographer(photographer))
-	const picture = `assets/photographers/${photographerModel.portrait}`
-	const photographHeader = getContElemCont('section', 'photograph-header')
-	const photographHeader__content = getContElemCont(
-		'div',
-		'photograph-header__content'
-	)
-	const photographHeader__title = textBlock('h2', photographerModel.name)
-	const contactBtn = getContElemCont('button', 'contact_button')
-	const photographHeader__location = textBlock('p', photographerModel.place)
-	const photographHeader__tag = textBlock('p', photographerModel.tagline)
-	const photographHeader__imgCont = getContElemCont(
-		'div',
-		'photograph-header__imgCont'
-	)
-	const photographerHeader_img = getImage(photographerModel.name, picture)
-
-	const photographHeader__info = getContElemCont(
-		'div',
-		'photograph-header__info'
-	)
-	const photographHeader__likes = getContElemCont(
-		'span',
-		'photograph-header__likes'
-	)
-	const heartIcon = getImage('likes', './assets/icons/heart.svg')
-	const photographMedia = getContElemCont('section', 'photograph-media')
-
-	// preparation
-	contactBtn.textContent = 'Contactez-moi'
-	contactBtn.setAttribute('onclick', 'displayModal()')
-	photographHeader__location.classList.add('photograph-header__location')
-	photographHeader__tag.classList.add('photograph-header__tag')
-	photographHeader__info.classList.add('photograph-header__info')
-
-	const spanLikes = document.createElement('span')
-	spanLikes.innerHTML = photographerModel.likes
-	photographHeader__likes.appendChild(spanLikes)
-	photographHeader__likes.appendChild(heartIcon)
-	const spanPrice = document.createElement('span')
-	spanPrice.textContent = photographerModel.price
-
-	// append
-	photographHeader__content.appendChild(photographHeader__title)
-	photographHeader__content.appendChild(photographHeader__location)
-	photographHeader__content.appendChild(photographHeader__tag)
-	photographHeader__imgCont.appendChild(photographerHeader_img)
-	photographHeader.appendChild(photographHeader__content)
-	photographHeader.appendChild(contactBtn)
-	photographHeader.appendChild(photographHeader__imgCont)
-	photographHeader__info.appendChild(photographHeader__likes)
-	photographHeader__info.appendChild(spanPrice)
-	photographHeader.appendChild(photographHeader__info)
-	mainCont.appendChild(photographHeader)
-	mainCont.appendChild(photographMedia)
-}
-
-*/
 function populateMediaScroll(filteredPhotos) {
 	const photographMedia = document.querySelector('.photograph-media')
 	const photographHeader__content = getContElemCont(
