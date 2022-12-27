2f1de37a2511063df9d97c5fa2e15d7bc3f90c3a
lostmart <lostmart@hotmail.com>
2022-12-27 11:50:00 +0100

    active item

--- scripts/factories/createDomElems.js
+++ scripts/factories/createDomElems.js
@@ -1,12 +1,12 @@
-let newCarousel = null
 let showCarousel = false
+let carousel = null
 
 // create an image
 // first arg: name(alt text) - second: image source(url)
 export function getImage(name, picture) {
-	const img = document.createElement("img")
-	img.setAttribute("src", picture)
-	img.setAttribute("alt", name)
+	const img = document.createElement('img')
+	img.setAttribute('src', picture)
+	img.setAttribute('alt', name)
 	return img
 }
 
@@ -28,39 +28,28 @@ export function getContElemCont(elemTyp, className) {
 export function photoCard(selectedPic, indx) {
 	const { image } = selectedPic
 
-	const link = document.createElement("a")
-	link.setAttribute("href", "javascript:void(0)")
+	const link = document.createElement('a')
+	link.setAttribute('href', 'javascript:void(0)')
 
 	const photographHeader__content = getContElemCont(
-		"article",
-		"photograph-media__card"
+		'article',
+		'photograph-media__card'
 	)
 
 	const photographMedia__imgCont = getContElemCont(
-		"div",
-		"photograph-media__imgCont"
+		'div',
+		'photograph-media__imgCont'
 	)
-	if (image) {
-		const imgUrl = `assets/${selectedPic.folderName()}${selectedPic.image}`
-		const img = getImage(selectedPic.title, imgUrl)
-		img.dataset.indxNo = indx
-		photographMedia__imgCont.appendChild(img)
-	} else {
-		const videoUrl = `assets/${selectedPic.folderName()}${selectedPic.video}`
-		const video = document.createElement("video")
-		const source = document.createElement("source")
-		source.src = videoUrl
-		video.appendChild(source)
-		photographMedia__imgCont.appendChild(video)
-	}
 
-	const photoMedia__text = getContElemCont("div", "photo-media__text")
-	photoMedia__text.appendChild(textBlock("h2", selectedPic.title))
-	const div = document.createElement("div")
-	div.appendChild(textBlock("span", selectedPic.likes))
+	createMediaUrl(selectedPic, image, indx, photographMedia__imgCont)
+
+	const photoMedia__text = getContElemCont('div', 'photo-media__text')
+	photoMedia__text.appendChild(textBlock('h2', selectedPic.title))
+	const div = document.createElement('div')
+	div.appendChild(textBlock('span', selectedPic.likes))
 	photoMedia__text.appendChild(div)
 
-	div.appendChild(getImage("likes", "./assets/icons/heart-red.svg"))
+	div.appendChild(getImage('likes', './assets/icons/heart-red.svg'))
 	photoMedia__text.appendChild(div)
 
 	photographHeader__content.appendChild(photographMedia__imgCont)
@@ -68,9 +57,9 @@ export function photoCard(selectedPic, indx) {
 
 	link.appendChild(photographHeader__content)
 
-	link.addEventListener("click", (e) => {
-		const mediaIndx = e.target.getAttribute("data-indx-no")
-		toggleCarousel()
+	link.addEventListener('click', (e) => {
+		const mediaIndx = e.target.getAttribute('data-indx-no')
+		toggleCarousel(mediaIndx)
 	})
 	// console.log(link)
 	return link
@@ -78,58 +67,83 @@ export function photoCard(selectedPic, indx) {
 
 // creates carousel
 
-export function createCarousel(elements, carousel) {
-	newCarousel = carousel
-
-	// console.log(elements)
-	const full_screen_media = getContElemCont("div", "full_screen_media")
+export function createCarousel(elements) {
+	const full_screen_media = getContElemCont('div', 'full_screen_media')
 	const full_screen_media__carousel = getContElemCont(
-		"div",
-		"full_screen_media__carousel"
+		'div',
+		'full_screen_media__carousel'
 	)
 	const full_screen_media__carouselInner = getContElemCont(
-		"div",
-		"full_screen_media__carousel-inner"
+		'div',
+		'full_screen_media__carousel-inner'
 	)
-	const carouselItem = getContElemCont("div", "carousel-item")
-	const carouselImg = getImage(
-		"teto",
-		"http://127.0.0.1:5501/assets/images/Mimi/Animals_Rainbow.jpg"
-	)
+	elements.forEach((pic) => {
+		const carouselItem = getContElemCont('div', 'carousel-item')
+		const { image } = pic
+		const carouselItemReady = createMediaUrl(pic, image, 0, carouselItem)
+		console.log(carouselItemReady)
+		full_screen_media__carouselInner.appendChild(carouselItemReady)
+	})
 
-	const closeBtn = getContElemCont("button", "full_screen_media__btn")
-	closeBtn.classList.add("full_screen_media__btn-close")
-	closeBtn.setAttribute("aria-label", "close")
-	const closeBtnImg = getImage("close carousel", "./assets/icons/close-red.svg")
-	closeBtn.addEventListener("click", () => console.log("nunca temas"))
+	const closeBtn = getContElemCont('button', 'full_screen_media__btn')
+	closeBtn.classList.add('full_screen_media__btn-close')
+	closeBtn.setAttribute('aria-label', 'close')
+	const closeBtnImg = getImage('close carousel', './assets/icons/close-red.svg')
+	closeBtn.addEventListener('click', () => toggleCarousel(carousel))
 
-	const leftLink = getContElemCont("a", "full_screen_media__btn")
-	leftLink.classList.add("full_screen_media__btn-left")
-	leftLink.setAttribute("aria-label", "previous image")
+	const leftLink = getContElemCont('a', 'full_screen_media__btn')
+	leftLink.classList.add('full_screen_media__btn-left')
+	leftLink.setAttribute('aria-label', 'previous image')
 
-	const rightLink = getContElemCont("a", "full_screen_media__btn")
-	rightLink.setAttribute("aria-label", "next image")
+	const rightLink = getContElemCont('a', 'full_screen_media__btn')
+	rightLink.setAttribute('aria-label', 'next image')
+	rightLink.addEventListener('click', () => console.log('right btn ...'))
 
 	closeBtn.appendChild(closeBtnImg)
-	carouselItem.appendChild(carouselImg)
-	full_screen_media__carouselInner.appendChild(carouselItem)
 	full_screen_media__carousel.appendChild(full_screen_media__carouselInner)
 	full_screen_media__carousel.appendChild(closeBtn)
 	full_screen_media__carousel.appendChild(leftLink)
 	full_screen_media__carousel.appendChild(rightLink)
 	full_screen_media.appendChild(full_screen_media__carousel)
-	full_screen_media.addEventListener("click", () => console.log("doing ..."))
-	// console.log(full_screen_media__carousel)
+	console.log(full_screen_media__carousel)
+	carousel = full_screen_media
 	return full_screen_media
 }
 
-function toggleCarousel(mediaIndx) {
-	console.log("running fn ...")
+export function toggleCarousel(mediaIndx) {
+	console.log(mediaIndx)
 	if (!showCarousel) {
-		newCarousel.style.display = "flex"
+		carousel.style.display = 'flex'
 		showCarousel = true
 	} else {
-		newCarousel.style.display = "none"
+		carousel.style.display = 'none'
 		showCarousel = false
+	}
+}
+
+function setActiveItem() {
+	console.log('acting ... ')
+}
+
+// creates a media holder an image or video
+function createMediaUrl(selectedPic, image, indx, element) {
+	if (image) {
+		const imgUrl = `assets/${selectedPic.folderName()}${selectedPic.image}`
+		const img = getImage(selectedPic.title, imgUrl)
+		img.dataset.indxNo = indx
+		element.appendChild(img)
+		return element
+	} else {
+		const videoUrl = `assets/${selectedPic.folderName()}${selectedPic.video}`
+		const video = document.createElement('video')
+		video.setAttribute('width', '350')
+		video.setAttribute('height', '300')
+		video.setAttribute('controls', true)
+		const source = document.createElement('source')
+		source.setAttribute('type', 'video/mp4')
+		source.src = videoUrl
+		video.appendChild(source)
+		element.appendChild(video)
+		return element
 	}
 }
